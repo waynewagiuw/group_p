2e0d49b345ba591192ce8da40a4b233fbf30ff78
tesdoang <s22110290@student.unklab.ac.id>
2023-10-17 21:36:10 +0800

    commit all

---
+++ src/components/Home/Card_info/index.jsx
@@ -0,0 +1,16 @@
+import Logo from "../../Header/Logo";
+import Evolution from "./Evolution";
+import Type_Effect from "./Type_Effect";
+
+const Card_info = () => {
+    return (
+        <>
+          <Evolution />
+          <Logo />
+          <Type_Effect />
+        </>
+      );
+  };
+  
+  export default Card_info;
+  
